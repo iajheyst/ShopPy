0c0005531a33eba2f5fc7b2be324fe051a674659
ionel.muresan <123Criminalistic>
2021-04-15 14:24:01 +0300

    Add User class.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from produs import Produs
+from user import User
 
 acer = Produs("Laptop acer 15.6'", 400, "Office")
 
 print(acer.pret)
+
+ionut=User("Ionut", "adresa x", "blahblah", "ionut@gmail.com")
+
+print(ionut.adresa)
---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    nume=""
+    adresa=""
+    password=""
+    email=""
+
+    def __init__(self, nume, adresa, password, email):
+        self.nume=nume
+        self.adresa=adresa
+        self.password=password
+        self.email=email
